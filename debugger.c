82806ee154c5ce455771d538ed88a08a74bcecb7
Collin Tod <squablesquable@gmail.com>
2018-03-15 16:56:57 -0400

    Documentation and todos

--- debugger.c
+++ debugger.c
@@ -6,38 +6,56 @@
 
 int main(int argc, char **argv){
 
+	//TODO: Add clarg checking
 	pid_t pid = (pid_t)strtol(argv[1], NULL, 10);
 	long offset = strtol(argv[2], NULL, 16);
 
-	kill(pid, SIGSTOP);
-
 	char memfilename[32];
 
+	// Generates the filename of the process's mem file.
 	sprintf(memfilename, "/proc/%d/mem", pid);
+
+	// Opens the mem file 
 	FILE *memfile = fopen(memfilename, "r");
 
-	// Make sure that the file was properly opened
+	// Make sure that the file was properly opened.
 	if(errno){
 		perror("fopen");
 		return 1;
 	}
 
-	char buf[33];
+	char buf[33]; // Buffer for reading from the mem file
 
-	// Seek to the correct offset in the file
+	// Stops the process temporarily to avoid race conditions.
+	kill(pid, SIGSTOP);
+
+	/*
+	 * Seek to the correct offset in the file
+	 * It is very important that the memory at offset is actually allocated. If
+	 * it is not allocated in the other process, then reading from the
+	 * /proc/PID/mem file will result in an EIO (i.e. an Input output error).
+	 */
 	fseek(memfile, offset, SEEK_SET);
 
+
+	// Reads a certain number (right now 32) bytes from the mem file
 	fgets(buf, 32, memfile);
 
+	// Makes sure that the read from the mem file was successful
 	if(errno){
 		perror("fgets");
 		return 1;
 	}
 
+	// TODO: make this more robust and able to handle different lengths of mem.
+	// Prints out the memory read from the file
 	for(int i = 0; i < 4; i++){
 		printf("%d", buf[i]);
 	}
+
+	// Continues the process, as it no longer needs to be stopped
 	kill(pid, SIGCONT);
+
 	return 0;
 
 }
--- tester.c
+++ tester.c
@@ -1,7 +1,19 @@
+/*
+ * tester.c: A program that simply loops forever, holding a piece of memory that
+ * can be changed and easily accessed by memory address. The purpose of this
+ * program is to provide a simple testing ground for debugger.c, i.e. a program
+ * with an easily identifiable memory segement that can be quickly identified as
+ * correct or not, and a way to show if the process has been stopped or not.
+ * Author: Collin Tod
+ *
+ * TODO: Add automatic addr dumping once memory is allocated
+ */
 #include <stdio.h>
 #include <stdlib.h>
 
 int main(int argc, char **argv){
+	//These two lines are the memoryplant that will be searched for. The address
+	//of this memory is in the file memoryplantaddr.txt
 	int *memoryplant = malloc(sizeof(int));
 	*memoryplant = argc;
 
