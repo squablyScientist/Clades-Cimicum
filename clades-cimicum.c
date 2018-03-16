6d08f2aae46e77adb0a1e342df12bd29393a96b5
Collin Tod <squablesquable@gmail.com>
2018-03-16 14:39:01 -0400

    Add ability to choose length
    The user may now choose the number of bytes to be read from the offset
    given. This will be useful for reading strings, as well as integers that
    exceed the value of 255, floats, longs, etc.

--- clades-cimicum.c
+++ clades-cimicum.c
@@ -1,6 +1,4 @@
-//TODO: segment functionality
 //TODO: implement an output format choice
-//TODO: implement a choice for how many bytes to read
 //TODO: add autocompletion for the names of processes
 //TODO: allow for '0x' at the beginning of memory address
 //TODO: add getopt options for various output formatting
@@ -13,31 +11,22 @@
  *
  * Author: Collin Tod
  */
-
+#include <string.h>
 #include <stdlib.h>
 #include <errno.h>
 #include <stdio.h>
 #include <signal.h>
 
 void usage(void){
-	fprintf(stderr, "Usage: debugger <PID> <mem_offset>\n");
+	fprintf(stderr, "Usage: debugger <PID> <mem_offset> <length>\n");
 }
 
-int main(int argc, char **argv){
-
-	if(argc != 3){
-		usage();
-		return EXIT_FAILURE;
-	}
-
-	pid_t pid = (pid_t)strtol(argv[1], NULL, 10);
-	long offset = strtol(argv[2], NULL, 16);
+int readmemory(char *buf, pid_t pid, long offset, size_t length){
 
 	char memfilename[32];
 
 	// Generates the filename of the process's mem file.
 	sprintf(memfilename, "/proc/%d/mem", pid);
-
 
 	// Opens the mem file 
 	FILE *memfile = fopen(memfilename, "r");
@@ -48,8 +37,6 @@ int main(int argc, char **argv){
 		return 1;
 	}
 
-	char buf[33]; // Buffer for reading from the mem file
-
 	// Stops the process temporarily to avoid race conditions.
 	kill(pid, SIGSTOP);
 
@@ -59,11 +46,10 @@ int main(int argc, char **argv){
 	 * it is not mapped in the other process, then reading from the
 	 * /proc/PID/mem file will result in an EIO (i.e. an Input output error).
 	 */
-	fseek(memfile, offset, SEEK_SET);
+	fseek(memfile, offset , SEEK_SET);
 
-
-	// Reads a certain number (right now 32) bytes from the mem file
-	fgets(buf, 32, memfile);
+	// Reads a certain number bytes from the mem file
+	fgets(buf, length+1, memfile);
 
 	// Makes sure that the read from the mem file was successful
 	if(errno){
@@ -71,16 +57,43 @@ int main(int argc, char **argv){
 		return 1;
 	}
 
+	// Close the memory file; it is no longer needed
 	fclose(memfile);
 
-	// TODO: make this more robust and able to handle different lengths of mem.
-	// Prints out the memory read from the file
-	for(int i = 0; i < 4; i++){
-		printf("%d", buf[i]);
+	//TODO: Find a way to do this that doesn't mess with SIGINT later
+	// Continues the process since it no longer needs to be stopped
+	kill(pid, SIGCONT);
+
+	return 0;
+}
+
+int main(int argc, char **argv){
+
+	// Makes sure that the correct number of clargs was entered
+	if(argc != 4){
+		usage();
+		return EXIT_FAILURE;
 	}
 
-	// Continues the process, as it no longer needs to be stopped
-	kill(pid, SIGCONT);
+	pid_t pid = (pid_t)strtol(argv[1], NULL, 10);
+	long offset = strtol(argv[2], NULL, 16);
+	int length = (int)strtol(argv[3], NULL, 10);
+
+	char buf[length+1]; //The buffer that will hold the memory read in 
+
+	// Reads the memory and makes sure that it was successful. If it wasn't exit
+	if(readmemory(buf, pid, offset, length) != 0){
+		return EXIT_FAILURE;
+	}
+
+	//TODO: make this able to handle different formats than just integers.
+	// Prints out the memory read from the file
+	
+	printf("Memory at %p: ", offset);
+	for(int i = 0; i < length; i++){
+		printf("%d", buf[i]);
+	}
+	printf("\n");
 
 	return 0;
 
